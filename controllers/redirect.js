d3b829f789c52abd8880fb3df528b194065db924
Aayush Sood <aayushsoodphp@gmail.com>
2024-08-31 15:44:56 +0530

    Added Fronted

--- controllers/redirect.js
+++ controllers/redirect.js
@@ -1,6 +1,7 @@
 const URL = require('../models/url');
 
 async function handleRedirectAndUpdate(req , res){
+    console.log("New request Received");
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
     {
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const staticRouter = require('./routes/staticRouter')
 const {connectToMongoDB} = require('./connection');
 const urlRoute = require('./routes/url')
 const analyticsRoute = require('./routes/redirect');
@@ -9,14 +11,19 @@ const PORT = 8001;
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(() => console.log('MongoDB Connected'))
 
+//Server Side Rendering
+app.set("view engine" , "ejs");
+app.set("viwes" , path.resolve("./views"));
+
 //MiddleWare - Plugin
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}));
 //Routes
-app.use("/url" , urlRoute);
+app.use("/url" , urlRoute); //post request for url and analytics
 
-app.use("/" , analyticsRoute);
+app.use("/shortid" , analyticsRoute); //redirectoin
 
+app.use("/" , staticRouter); //HOme page to accept data
 
 //Server Start
 app.listen(PORT , () => console.log('Server started at PORT:', PORT))
